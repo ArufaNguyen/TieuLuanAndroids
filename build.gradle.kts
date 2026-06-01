e19be98a8a688a8222e6087b0ac1099c6cdb6112
Aru <lamnguyetda1234@gmail.com>
2026-06-01 16:27:56 +0700

    fix: config update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.3.5" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "TieuLuanAndroids"
 include(":app")
- 
+include(":backend")
+project(":backend").projectDir = file("backend/SmartCalendarAPI")
